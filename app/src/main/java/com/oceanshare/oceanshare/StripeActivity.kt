2faae07b8cdf041b0c9f651729cccc3402a2170e
DylanCastillon <dylan.castillon@epitech.eu>
2019-12-13 02:46:19 +0100

    stripe payment added

--- app/src/main/java/com/oceanshare/oceanshare/StripeActivity.kt
+++ app/src/main/java/com/oceanshare/oceanshare/StripeActivity.kt
@@ -4,45 +4,138 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
-import com.google.gson.GsonBuilder
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
 import com.stripe.android.ApiResultCallback
 import com.stripe.android.PaymentConfiguration
-import com.stripe.android.PaymentIntentResult
 import com.stripe.android.Stripe
-import com.stripe.android.model.ConfirmPaymentIntentParams
-import com.stripe.android.model.StripeIntent
 import com.stripe.android.model.Token
 import com.stripe.android.view.CardInputWidget
 import kotlinx.android.synthetic.main.activity_stripe.*
-import java.lang.ref.WeakReference
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody.Companion.toRequestBody
 import org.json.JSONObject
 import java.io.IOException
 
+class SubscribeUser {
+    var type : Int? = null
+    var start : Long? = null
+    var end : Long? = null
+}
 
 class StripeActivity : AppCompatActivity() {
 
     private val backendUrl = "https://us-central1-oceanshare-1519985626980.cloudfunctions.net/"
     private val publishableKey = "pk_test_aKG5XmyrMWd17loRBt4W45Vd00nDvn7UF1"
+    private var price  = 0
+    private var paymentType  = 0
     private val httpClient = OkHttpClient()
     private lateinit var stripe: Stripe
+    private val subscribeUser = SubscribeUser()
+    private val fbAuth = FirebaseAuth.getInstance()
+    private val database = FirebaseDatabase.getInstance().reference
+    private val currentUser = fbAuth.currentUser?.uid.toString()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stripe)
+        getActualUserData()
+        manageViews()
+        payment()
+    }
 
+    private fun manageViews() {
+        if (paymentType != 0)
+            successedPayment()
+        else
+            paymentPage()
+    }
+
+    private fun paymentPage() {
+        one_day_offer.setOnClickListener {
+            offer_page.visibility = View.GONE
+            payment_page.visibility = View.VISIBLE
+            custom_duration_message.text = getString(R.string.subscription_one_day_message)
+            price = 2
+            subscribeUser.type = 1
+        }
+        two_days_offer.setOnClickListener {
+            offer_page.visibility = View.GONE
+            payment_page.visibility = View.VISIBLE
+            custom_duration_message.text = getString(R.string.subscription_two_days_message)
+            price = 3
+            subscribeUser.type = 2
+        }
+        one_month_offer.setOnClickListener {
+            offer_page.visibility = View.GONE
+            payment_page.visibility = View.VISIBLE
+            custom_duration_message.text = getString(R.string.subscription_one_month_message)
+            price = 10
+            subscribeUser.type = 3
+        }
+    }
+
+    private fun successedPayment() {
+
+        runOnUiThread {
+            offer_page.visibility = View.GONE
+            payment_page.visibility = View.GONE
+            payment_confirmation_message.visibility = View.VISIBLE
+
+            if (subscribeUser.type != null) {
+                val inputMethodManager = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputMethodManager.hideSoftInputFromWindow(View(this).windowToken, 0)
+                cardInputWidget.clear()
+                subscribeUser.start = System.currentTimeMillis() / 1000
+                subscribeUser.end = System.currentTimeMillis() / 1000 + 2592000 //TODO Change to real month
+                database.child("users").child(currentUser).child("sub").setValue(subscribeUser)
+
+                Handler().postDelayed({
+                    val mainActivityIntent = Intent(this, MainActivity::class.java)
+                    startActivity(mainActivityIntent)
+                }, 5000)
+
+            }
+        }
+    }
+
+    private fun getActualUserData() {
+        database.child("users").child(currentUser).addChildEventListener(
+                object : ChildEventListener {
+                    override fun onChildAdded(p0: DataSnapshot, p1: String?) {
+                        println(p0.child("preferences").value)
+                        if (p0.child("sub").child("type").exists()) {
+                            println("gros chibrax")
+                            paymentType = p0.child("sub").child("type").value.toString().toInt()
+                        }
+                    }
+
+                    override fun onChildChanged(p0: DataSnapshot, s: String?) {}
+                    override fun onChildRemoved(p0: DataSnapshot) {}
+                    override fun onChildMoved(p0: DataSnapshot, s: String?) {}
+                    override fun onCancelled(p0: DatabaseError) {}
+
+                })
+    }
+
+    private fun payment() {
         // Configure the SDK with your Stripe publishable key so that it can make requests to the Stripe API
         // ⚠️ Don't forget to switch this to your live-mode publishable key before publishing your app
         PaymentConfiguration.init(publishableKey) // Get your key here: https://stripe.com/docs/keys#obtain-api-keys
 
         // Hook up the pay button to the card widget and stripe instance
         val payButton: Button = findViewById(R.id.payButton)
-        val weakActivity = WeakReference<Activity>(this)
         payButton.setOnClickListener {
             // Get the card details from the card widget
             val cardInputWidget =
@@ -69,20 +162,21 @@ class StripeActivity : AppCompatActivity() {
                         httpClient.newCall(request)
                                 .enqueue(object: Callback {
                                     override fun onFailure(call: Call, e: IOException) {
-                                        displayAlert(weakActivity.get(), "Failed to decode response from server", "Error: $e")
+                                        println("Failed to decode response from server")
                                     }
 
                                     override fun onResponse(call: Call, response: Response) {
                                         if (!response.isSuccessful) {
-                                            displayAlert(weakActivity.get(), "Failed to decode response from server", "Error: $response")
+                                            println("Failed to decode response from server")
                                         } else {
                                             val responseData = response.body?.string()
                                             var responseJSON = JSONObject(responseData)
                                             val error = responseJSON.optString("error", null)
                                             if (error != null) {
-                                                displayAlert(weakActivity.get(), "Payment failed", error)
+                                                println("Payment failed")
                                             } else {
-                                                displayAlert(weakActivity.get(), "Success", "Payment succeeded!", true)
+                                                println("Payment succeeded!")
+                                                successedPayment()
                                             }
                                         }
                                     }
@@ -90,35 +184,11 @@ class StripeActivity : AppCompatActivity() {
                     }
 
                     override fun onError(e: java.lang.Exception) {
-                        displayAlert(weakActivity.get(), "Error", e.localizedMessage)
+                        println("Error" + e.localizedMessage)
                     }
                 })
             }
 
         }
     }
-
-    private fun displayAlert(activity: Activity?, title: String, message: String, restartDemo: Boolean = false) {
-        if (activity == null) {
-            return
-        }
-        runOnUiThread {
-            val builder = AlertDialog.Builder(activity)
-            builder.setTitle(title)
-            builder.setMessage(message)
-            if (restartDemo) {
-                builder.setPositiveButton("Restart demo") { _, _ ->
-                    val cardInputWidget =
-                            findViewById<CardInputWidget>(R.id.cardInputWidget)
-                    cardInputWidget.clear()
-                }
-            }
-            else {
-                builder.setPositiveButton("Ok", null)
-            }
-            val dialog = builder.create()
-            dialog.show()
-        }
-    }
-
 }
